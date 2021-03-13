962a472b7b8e34183a0f95bb388988cc2b40b83c
Serhii <67795802+seven8959@users.noreply.github.com>
2021-03-13 19:50:08 +0200

    task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+exports.min = function min(array) {
+  return array && array.length ? Math.min(...array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  return array && array.length ? Math.max(...array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  return array && array.length
+    ? array.reduce((r, v) => r + v) / array.length
+    : 0;
+};
